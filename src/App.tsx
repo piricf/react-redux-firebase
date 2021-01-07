6ea626548c970d2d592aa8af575cb1843a58fd72
piricf <piricf9@gmail.com>
2021-01-07 11:56:25 +0100

    Navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePageView from './views/HomePageView';
+import Navbar from './components/Navbar';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
+        <Navbar />
         <Route path="/" exact component={HomePageView} />
       </Switch>
     </Router>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import { AiOutlineCar } from "react-icons/ai";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../css/Navbar.css"
+
+const Navbar: React.FC = () => {
+   const [click, setClick] = useState(false)
+   const [button, setButton] = useState(true)
+    
+    const closeMobileMenu = () => setClick(false)
+
+    const handleClick = (e: any) => {
+        e.preventDefault()
+        setClick(!click)
+    }
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+
+    window.addEventListener("resize", showButton)
+
+    return (
+        <>
+            <div className="navbar">
+                <div className="navbar-container container">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                        <AiOutlineCar className="navbar-icon" />
+                        VEHICLES
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        {click ? <FaTimes/> : <FaBars />}
+                    </div>
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Home 
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/audi" className="nav-links" onClick={closeMobileMenu}>
+                              Audi
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/bmw" className="nav-links" onClick={closeMobileMenu}>
+                                Bmw
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/mercedes" className="nav-links" onClick={closeMobileMenu}>
+                                Mercedes
+                            </Link>
+                        </li>
+                        <li className="nav-btn">
+                            <Link to="/sign-up" className="btn-link">
+                                <button  onClick={closeMobileMenu} >Sign Up</button>
+                                {button}
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
